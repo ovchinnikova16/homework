bc02033774a61d6756a943e81feb558baa6dae70
ovchinnikova16 <32325202+ovchinnikova16@users.noreply.github.com>
2017-10-23 18:20:30 +0300

    Add files via upload

---
+++ HomeExercises/NumberValidatorTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace HomeExercises
+{
+
+	public class NumberValidatorTests
+    { 
+	    [TestCase(17, 2, true, null, ExpectedResult = false)]
+	    [TestCase(17, 2, true, "", ExpectedResult = false)]
+	    [TestCase(3, 2, true, "a.sd", ExpectedResult = false)]
+	    [TestCase(17, 2, true, "1", ExpectedResult = true)]
+	    [TestCase(17, 2, true, "1.2", ExpectedResult = true)]
+	    [TestCase(17, 2, true, "+1.23", ExpectedResult = true)]
+	    [TestCase(17, 2, false, "-1.23", ExpectedResult = true)]
+	    [TestCase(3, 2, true, "12.34", ExpectedResult = false)]
+	    [TestCase(3, 2, false, "-1.23", ExpectedResult = false)]
+	    [TestCase(3, 2, true, "+1.23", ExpectedResult = false)]
+	    [TestCase(17, 2, true, "1.234", ExpectedResult = false)]
+	    [TestCase(17, 2, true, "-1.23", ExpectedResult = false)]
+	    public static bool ValidateNumber(int precision, int scale, bool onlyPositive, string value)
+	    {
+	        return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
+	    }
+
+        [TestCase(-1, 2, true, TestName = "When presision is negative")]
+        [TestCase(2, 3, true, TestName = "When scale bigger than presision")]
+        [TestCase(1, -1, true, TestName = "When scale is negative")]
+        public static void Throw(int precision, int scale, bool onlyPositive)
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(precision, scale, onlyPositive));
+        }
+    }
+
+	public class NumberValidator
+	{
+		private readonly Regex numberRegex;
+		private readonly bool onlyPositive;
+		private readonly int precision;
+		private readonly int scale; 
+
+		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
+		{
+			this.precision = precision;
+			this.scale = scale;
+			this.onlyPositive = onlyPositive;
+			if (precision <= 0)
+				throw new ArgumentException("precision must be a positive number");
+			if (scale < 0 || scale >= precision)
+				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
+			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+		}
+
+		public bool IsValidNumber(string value)
+		{
+			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
+			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
+			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
+			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+
+			if (string.IsNullOrEmpty(value))
+				return false;
+
+			var match = numberRegex.Match(value);
+			if (!match.Success)
+				return false;
+
+			// Знак и целая часть
+			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+			// Дробная часть
+			var fracPart = match.Groups[4].Value.Length;
+
+			if (intPart + fracPart > precision || fracPart > scale)
+				return false;
+
+			if (onlyPositive && match.Groups[1].Value == "-")
+				return false;
+			return true;
+		}
+	}
+}
---
+++ HomeExercises/ObjectComparison.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace HomeExercises
+{
+	public class ObjectComparison
+	{
+		[Test]
+		[Description("Проверка текущего царя")]
+		[Category("ToRefactor")]
+		public void CheckCurrentTsar()
+		{
+			var actualTsar = TsarRegistry.GetCurrentTsar();
+
+			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+
+            // Перепишите код на использование Fluent Assertions.
+            actualTsar.Name.Should().Be(expectedTsar.Name);
+            actualTsar.Age.Should().Be(expectedTsar.Age);
+            actualTsar.Height.Should().Be(expectedTsar.Height);
+            actualTsar.Weight.Should().Be(expectedTsar.Weight);
+
+            actualTsar.Parent.Name.Should().Be(expectedTsar.Parent.Name);
+            actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
+            actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
+            actualTsar.Parent.Weight.Should().Be(expectedTsar.Parent.Weight);
+
+            //actualTsar.ShouldBeEquivalentTo(expectedTsar);
+        }
+
+		[Test]
+		[Description("Альтернативное решение. Какие у него недостатки?")]
+		public void CheckCurrentTsar_WithCustomEquality()
+		{
+			var actualTsar = TsarRegistry.GetCurrentTsar();
+			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+			new Person("Vasili III of Russia", 28, 170, 60, null));
+
+			// Какие недостатки у такого подхода? 
+			Assert.True(AreEqual(actualTsar, expectedTsar));
+
+		}
+
+		private bool AreEqual(Person actual, Person expected)
+		{
+			if (actual == expected) return true;
+			if (actual == null || expected == null) return false;
+			return
+			actual.Name == expected.Name
+			&& actual.Age == expected.Age
+			&& actual.Height == expected.Height
+			&& actual.Weight == expected.Weight
+			&& AreEqual(actual.Parent, expected.Parent);
+		}
+	}
+
+	public class TsarRegistry
+	{
+		public static Person GetCurrentTsar()
+		{
+			return new Person(
+				"Ivan IV The Terrible", 54, 170, 70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+		}
+	}
+
+	public class Person
+	{
+		public static int IdCounter = 0;
+		public int Age, Height, Weight;
+		public string Name;
+		public Person Parent;
+		public int Id;
+
+		public Person(string name, int age, int height, int weight, Person parent)
+		{
+			Id = IdCounter++;
+			Name = name;
+			Age = age;
+			Height = height;
+			Weight = weight;
+			Parent = parent;
+		}
+	}
+}
